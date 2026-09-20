b9065ffebea1c8454ef29d47750eabf2bfe1a1c6
106509354 <106509454@student.swin.edu.aum>
2026-09-20 13:43:48 +1000

    removed html file, reinterpreted instructions, should be as instructed.

--- myfirst.php
+++ myfirst.php
@@ -1,6 +1,18 @@
-<?php
-        $marks = array(85, 85, 95);
+<!DOCTYPE html>
+<html lang = "en">
 
+<head>
+    <meta charset = "utf-8">
+    <title>Using PHP Variables, arrays and operators</title>
+    <meta name="description" content="PHP" >
+    <meta name="keywords" content="PHP" >
+    <meta name="author" content="Ryan F"  >
+</head>
+
+<body>
+    <h1> PHP Variables, Arrays and operators </h1>
+    <?php
+        $marks[] = array(85, 85, 95);
         $marks[1] = 90; 
 
         $sum = array_sum($marks);
@@ -15,3 +27,6 @@
         echo $status . " Average: " . $avg
 
     ?>
+</body>
+
+</html>
