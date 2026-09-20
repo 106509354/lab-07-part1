1db10263cfe9bd5c7fb1203e9fbbae50fa650fdb
106509354 <106509454@student.swin.edu.aum>
2026-09-20 13:45:44 +1000

    html added to the daysarray.php file, completing php code of the task now.

--- daysarray.php
+++ daysarray.php
@@ -1,5 +1,19 @@
-<?php
-        $marks = array(85, 85, 95);
+<!DOCTYPE html>
+<html lang = "en">
 
+<head>
+    <meta charset = "utf-8">
+    <title>Using PHP Variables, arrays and operators</title>
+    <meta name="description" content="PHP" >
+    <meta name="keywords" content="PHP" >
+    <meta name="author" content="Ryan F"  >
+</head>
 
-?>
+<body>
+    <h1> PHP Variables, Arrays and operators </h1>
+    <?php
+    
+    ?>
+</body>
+
+</html>
